70ffbdbd75708646a42b3b7a474a4bcc24eba1e2
Voyager2718 <voyager2718@gmail.com>
2017-04-27 15:59:43 +0800

    Change main.cpp for better experience

--- main.cpp
+++ main.cpp
@@ -3,11 +3,18 @@
 
 using std::cout;
 using std::cin;
+using std::endl;
 
 int main(int argc, char *argv[]){
     GeneralBugAnalyser analyser;
     analyser.add_analyst(GeneralBugAnalyser::analyse);
-    shared_ptr<Report> report = analyser.analyse_log("./inst.log");
-    
+    if(argc <= 1){
+        cout<<"Enter a log path to analyse:"<<endl;
+        string path;
+        cin>>path;
+        shared_ptr<Report> report = analyser.analyse_log(path);
+    }else{
+        shared_ptr<Report> report = analyser.analyse_log(argv[1]);
+    }
     return 0;
 }
