77b57a6cfee25ee2183ce16307255b1bb9cb960f
Voyager2718 <voyager2718@gmail.com>
2017-04-28 15:07:26 +0800

    Finished all states
    Finished all specific states; Need to ignore normal codes

--- AnalyserFSM.cpp
+++ AnalyserFSM.cpp
@@ -70,7 +70,7 @@ private:
     bool is_virtual_tokenizer(){
         if(code.substr(code_position, 7) == "virtual"){
             code_position += 7;
-            tokens.push_back(make_map("virtual_key", "\"virtual\""));
+            tokens.push_back(make_map("virtual_identifier", "\"virtual\""));
             return true;
         }
         return false;
@@ -79,18 +79,17 @@ private:
     bool is_permission_tokenizer(){
         if(code.substr(code_position, 6) == "public"){
             code_position += 6;
-            tokens.push_back(make_map("permission_identifier", "public"));
+            tokens.push_back(make_map("permission_identifier", "\"public\""));
             return true;
         }else if(code.substr(code_position, 9) == "protected"){
             code_position += 9;
-            tokens.push_back(make_map("permission_identifier", "protected"));
+            tokens.push_back(make_map("permission_identifier", "\"protected\""));
             return true;
         }else if(code.substr(code_position, 7) == "private"){
             code_position += 7;
-            tokens.push_back(make_map("permission_identifier", "private"));
+            tokens.push_back(make_map("permission_identifier", "\"private\""));
             return true;
         }
-
         return false;
     }
 
